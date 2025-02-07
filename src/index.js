7e0c03549e4339464d8c89bcd1c93e3e5903631b
coiilee <dakyung96@gmail.com>
2025-02-07 11:52:44 +0900

    function, const

--- src/index.js
+++ src/index.js
@@ -5,20 +5,28 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ChapUseState from "./Chapter/ChapUseState";
 import PracticeUseState from "./Practice/PracticeUseState";
+import ChapProps from "./Chapter/ChapProps";
+import PracticeProps from "./Practice/PracticeProps"
+import ChapArrowFunction from "./Chapter/ChapArrowFunction";
+import ChapPropsFunction from "./Chapter/ChapPropsFunction";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* return 내에 사용할 수 있는 주석
+    <React.StrictMode>
+        {/* return 내에 사용할 수 있는 주석
 
-    App.js 는 필수로 사용해야 하는 자바스크립트 파일이 아님
-    만일 App.js가 아니라 다른 파일을 사용하길 원한다면
-    파일명.js를 import 해서 사용할 수 있음
-    */}
-    <App/>
-  <ChapUseState/>
-      <PracticeUseState/>
-  </React.StrictMode>
+        App.js 는 필수로 사용해야하는 자바스크립트 파일이 아님
+        만일 App.js 가 아니라 다른 파일을 사용하길 원한다면
+        파일명.js 를 import 해서 사용할 수 있음
+        */}
+        <App/>
+            <ChapPropsFunction/>
+        {/*<ChapUseState/>*/}
+        {/*<PracticeUseState />*/}
+        {/*<ChapProps />*/}
+        {/*<PracticeProps/>*/}
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
